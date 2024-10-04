f9b6aa5b34402c76ff781a08bd37f576e8f07d59
Tanuj <Tanuj@cmu.edu>
2024-10-04 10:28:09 -0700

    Remove dependency for pytz for friendly timestamps

--- utils.py
+++ utils.py
@@ -1,15 +1,24 @@
 from datetime import datetime
-import pytz
+from zoneinfo import ZoneInfo
 
 
 def convert_to_pst(timestamp, safety=True):
+    """
+    Converts a given timestamp to Pacific Standard Time (PST).
+
+    Args:
+    - timestamp (str): The timestamp to convert, in the format YYYY-MM-DDTHH:MM:SS.ssssssZ.
+    - safety (bool): If True, the function will return the original timestamp if it does not end with 'Z'. Defaults to True.
+
+    Returns:
+    - str: The converted timestamp in the format MM/DD/YY HH:MM AM/PM.
+    """
     if safety:
         if not timestamp.endswith('Z'):
+            # NOTE: Should I have a warning message?
             return timestamp
     dt = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-    dt_pst = dt.replace(
-        tzinfo=pytz.utc).astimezone(
-        pytz.timezone('US/Pacific'))
+    dt_pst = dt.replace(tzinfo=ZoneInfo('UTC')).astimezone(ZoneInfo('America/Los_Angeles'))
     return dt_pst.strftime("%m/%d/%y %I:%M%p")
 
 
