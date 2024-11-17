f8f025679fca47088bc2a922fffb2288b3d23535
Tanuj <Tanuj@cmu.edu>
2024-11-17 00:29:55 -0800

    Update constants.py

--- utils/owui_utils/constants.py
+++ utils/owui_utils/constants.py
@@ -1,7 +1,7 @@
 # System Messages
-TOOL_SYSTEM_MESSAGE = """You are a helpful assistant that can access external functions. When performing searches, consider the current date and time, which is {current_time}. When appropriate, create a short search_substring to narrow down the search results."""
+TOOL_SYSTEM_MESSAGE = """You are a helpful assistant that can access external functions. When performing searches, consider the current date and time, which is {current_time} (ISO format). If specified, create a single-word search_substring."""
 
-JSON_SYSTEM_MESSAGE = """You are a helpful assistant. You will parse a user query to construct search parameters to search for chunks (audio, ocr, etc.) in ScreenPipe's local database.
+JSON_SYSTEM_MESSAGE = """You are a helpful assistant. Given a user request, construct search parameters for searching chunks (audio, ocr, etc.) in ScreenPipe's local database.
 
 Use the properties field below to construct the search parameters:
 {schema}
@@ -9,7 +9,8 @@ Use the properties field below to construct the search parameters:
 Ensure the following rules are met:
     - limit must be between 1 and 100. defaults to 5 if not specified.
     - content_type must be one of: "ocr", "audio", "all"
-    - time values should be null or in ISO format relative to the current timestamp: {current_time}
+    - if provided, time values should be in ISO format relative to the current timestamp: {current_time}
+    - when appropriate, create a short search_substring to narrow down the search results
 
 Example search JSON objects:
 {examples}
@@ -17,13 +18,11 @@ Example search JSON objects:
 ONLY Output the search JSON object, nothing else.
 """
 
-FINAL_RESPONSE_SYSTEM_MESSAGE = """You are a helpful assistant that parses screenpipe search results. Use the search results to answer the user's question as best as possible. If unclear, synthesize the context and provide an explanation."""
-ALT_FINAL_RESPONSE_SYSTEM_MESSAGE = """You analyze all types of data from screen recordings and audio transcriptions. The user's query is designed to filter the search results. Provide comprehensive insights of the provided data."""
-
 EXAMPLE_SEARCH_JSON = """\
 {
-    "limit": 2,
+    "limit": 10,
     "content_type": "audio",
+    "search_substring": "jason",
     "start_time": null,
     "end_time": null
 }
